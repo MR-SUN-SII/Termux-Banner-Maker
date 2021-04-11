ef0e33577798fd7ba515992770e7637bfcfef46c
Saiful Islam Imon <82077660+MR-SUN-SII@users.noreply.github.com>
2021-04-11 16:34:14 +0600

    Update t-require.sh

--- t-require.sh
+++ t-require.sh
@@ -1,4 +1,35 @@
-z="
-";DBz='arix';uz=' Dev';az='at';Bz='r';qz='...!';FBz='8h4i';Oz=' Ins';nz='ash ';pz='n.sh';Mz='et B';Cz='apt ';tz='[32m';hz='echo';Xz='ruby';xz='y :\';Kz='et -';Gz='ade';CBz='ya P';EBz='it (';Iz='all ';Yz='gem ';GBz=')"';Wz=' -y';Rz='n';Lz='y';bz='f bi';Vz='nano';yz='e[33';oz='t-ba';BBz='tari';Sz='toil';vz='elop';Qz='atio';gz='cat';Hz='inst';ez='!! |';Nz='asic';wz='ed b';mz='un b';jz='"\e[';fz=' lol';lz='ow R';Jz='figl';Pz='tall';iz=' -e ';rz='!!"';Zz='lolc';ABz='m Su';Az='clea';Dz='upda';Tz='cows';kz='1m N';sz='1m\e';Ez='te';dz='ne !';Uz='ay -';Fz='upgr';cz='g Do';
-eval "$Az$Bz$z$Cz$Dz$Ez$z$Cz$Fz$Gz$z$Cz$Hz$Iz$Jz$Kz$Lz$z$Jz$Mz$Nz$Oz$Pz$Qz$Rz$z$Cz$Hz$Iz$Sz$Kz$Lz$z$Cz$Hz$Iz$Tz$Uz$Lz$z$Cz$Hz$Iz$Vz$Wz$z$Cz$Hz$Iz$Xz$Wz$z$Yz$Hz$Iz$Zz$az$z$Jz$Kz$bz$cz$dz$ez$fz$gz$z$hz$z$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$z$hz$z$hz$iz$jz$sz$tz$uz$vz$wz$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$GBz"
-a
+#!/bin/bash
+#
+#
+# Termux Banner Maker
+# Banner by Saiful Islam Imon(MR.SUN)
+# _____Do Not Copy Or Modify_____
+#
+#
+clear
+figlet -f big Termux Banner | lolcat
+cyan='\e[0;36m'
+lightgreen='\e[1;32m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+echo -e $lightgreen "\e[1m                Security Help For Ethical Hackers... "
+echo " "
+echo -e $yellow "\e[1m                         -Saiful Islam Imon(MR.SUN)"
+echo " "
+echo " "
+echo " "
+echo -e "\e[1m\e[33m\nWhat is Your \e[31mBanner \e[33mName\e[32m :\n\n"
+read varbanner
+echo
+echo -e "\e[1m\e[33m\nWhat is Your Cowsay Name\e[32m :\n\n "
+read varcowsay
+echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
+echo "toilet -f big '    $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo
+echo -e $lightpink "\e[1m  -----Congratulaions, now you have a banner in Termux Home page-----
+echo " "
+echo "clear" > cl34r.txt
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+ls
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/zshrc
